8c5a71772ca2e25c33c6c708dc03140d04d36848
Dania <Dania2008@list.ru>
2022-10-05 10:42:10 +0300

    Added Task034

---
+++ Task034/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив заполненный случайными положительными трехзначными
+// числами. Напишите программу, которая покажет количество четных чисел 
+// в массиве.
+Console.Clear();
+
+void InputArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i ++)
+        array[i] = new Random().Next(100,1000);
+}
+
+int ReleaseArray(int[]array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i ++)
+    {
+        if (array[i] % 2 == 0)
+        count ++;
+    }   
+    return count;
+}
+
+Console.WriteLine("Введите количество чисел в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int []array = new int [n];
+InputArray(array);
+Console.WriteLine("[" + string.Join(",", array) + "]");
+ReleaseArray(array);
+Console.WriteLine($"Количество четных чисел в массиве равно {ReleaseArray(array)}");
+
+
